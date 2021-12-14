3d36bf9d0c2652fe9e6201ff8ee584a39421e651
shinzanmono <shinzanmono1192@gmail.com>
2021-12-15 00:51:16 +0900

    finished https://developer.mozilla.org/ja/docs/Learn/Server-side/Express_Nodejs/Displaying_data/Book_detail_page

--- locallibrary-website/controllers/bookController.js
+++ locallibrary-website/controllers/bookController.js
@@ -43,36 +43,58 @@ exports.book_list = function(req, res, next) {
 
 // Display detail page for a specific book.
 exports.book_detail = function(req, res) {
-    res.send('NOT IMPLEMENTED: Book detail: ' + req.params.id);
+  async.parallel(
+    {
+      book: function(callback) {
+        Book.findById(req.params.id)
+          .populate('author')
+          .populate('genre')
+          .exec(callback)
+      },
+      book_instance: function(callback) {
+        BookInstance.find({ 'book': req.params.id })
+          .exec(callback);
+      },
+    }, function(err, results) {
+      if (err) { return next(err); }
+      if (results.book == null) {
+        var err = new Error('Book not found');
+        err.status = 404;
+        return next(err);
+      }
+      // successful, so render
+      res.render('book_detail', { title: 'Title', book: results.book, book_instances: results.book_instance });
+    }
+  );
 };
 
 // Display book create form on GET.
 exports.book_create_get = function(req, res) {
-    res.send('NOT IMPLEMENTED: Book create GET');
+  res.send('NOT IMPLEMENTED: Book create GET');
 };
 
 // Handle book create on POST.
 exports.book_create_post = function(req, res) {
-    res.send('NOT IMPLEMENTED: Book create POST');
+  res.send('NOT IMPLEMENTED: Book create POST');
 };
 
 // Display book delete form on GET.
 exports.book_delete_get = function(req, res) {
-   res.send('NOT IMPLEMENTED: Book delete GET');
+  res.send('NOT IMPLEMENTED: Book delete GET');
 };
 
 // Handle book delete on POST.
 exports.book_delete_post = function(req, res) {
-    res.send('NOT IMPLEMENTED: Book delete POST');
+  res.send('NOT IMPLEMENTED: Book delete POST');
 };
 
 // Display book update form on GET.
 exports.book_update_get = function(req, res) {
-    res.send('NOT IMPLEMENTED: Book update GET');
+  res.send('NOT IMPLEMENTED: Book update GET');
 };
 
 // Handle book update on POST.
 exports.book_update_post = function(req, res) {
-    res.send('NOT IMPLEMENTED: Book update POST');
+  res.send('NOT IMPLEMENTED: Book update POST');
 };
 

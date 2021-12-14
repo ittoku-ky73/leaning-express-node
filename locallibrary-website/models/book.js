7e4069d8ec5d3eb9d8b8fb0f5dd8de5082ea0247
shinzanmono <shinzanmono1192@gmail.com>
2021-12-15 00:32:57 +0900

    finished https://developer.mozilla.org/ja/docs/Learn/Server-side/Express_Nodejs/Displaying_data/Genre_detail_page

--- locallibrary-website/models/book.js
+++ locallibrary-website/models/book.js
@@ -5,6 +5,7 @@ let Schema = mongoose.Schema;
 let BookSchema = new Schema({
   title: {type: String, required: true},
   author: {type: Schema.ObjectId, ref: 'Author', required: true},
+  summary: {type: String, required: true},
   isbn: {type: String, required: true},
   genre: [{type: Schema.ObjectId, ref: 'Genre'}],
 });

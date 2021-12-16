f5df3e4a13f158a96741bba443d3e3e05d549035
shinzanmono <shinzanmono1192@gmail.com>
2021-12-17 04:29:01 +0900

    finished https://developer.mozilla.org/en-US/docs/Learn/Server-side/Express_Nodejs/forms/Create_author_form

--- locallibrary-website/controllers/authorController.js
+++ locallibrary-website/controllers/authorController.js
@@ -1,6 +1,7 @@
 const Author = require('../models/author');
 const async = require('async');
 const Book = require('../models/book');
+const { body, validationResult } = require('express-validator');
 
 // Display list of all Authors
 exports.author_list = function(req, res) {
@@ -44,13 +45,63 @@ exports.author_detail = function(req, res) {
 
 // Display Author create from on GET
 exports.author_create_get = function(req, res) {
-  res.send('NOT IMPLEMENTED: AUTHOR create GET');
+  res.render('author_form', { title: 'Create Author' });
 };
 
 // Handle Author create on POST
-exports.author_create_post = function(req, res) {
-  res.send('NOT IMPLEMENTED: AUTHOR create POST');
-};
+exports.author_create_post = [
+  // validate and sanitize the name field
+  body('first_name')
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage('First name must be specified')
+    .isAlphanumeric()
+    .withMessage('First name has non-alphanumeric characters'),
+  body('family_name')
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage('Family name must be specified')
+    .isAlphanumeric()
+    .withMessage('Family name has non-alphanumeric characters'),
+  body('date_of_birth', 'Invalid date of birth')
+    .optional({ checkFalsy: true })
+    .isISO8601()
+    .toDate(),
+  body('date_of_death', 'Invalid date of death')
+    .optional({ checkFalsy: true })
+    .isISO8601()
+    .toDate(),
+  // process request after validation and sanitization
+  function(req, res, next) {
+    // Extract the validation errors from a request
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      // there are errors. render the form again with sanitized values/error messages
+      res.render('author_form', {
+        title: 'Create Author',
+        author: req.body,
+        errors: errors.array()
+      });
+      return;
+    }
+
+    // create an author object with escaped and trimmed data
+    var author = new Author({
+      first_name: req.body.first_name,
+      family_name: req.body.family_name,
+      date_of_birth: req.body.date_of_birth,
+      date_of_death: req.body.date_of_death
+    });
+    author.save(function(err) {
+      if (err) { return next(err); }
+      // genre saved. redirect to genre detail page
+      res.redirect(author.url);
+    });
+  }
+];
 
 // Display Author delete from on GET
 exports.author_delete_get = function(req, res) {
